dcf3addb5dfcc055bdd21af7babe2960ed658e3c
JingYuan <jingyuan0926@gmail.com>
2025-08-03 04:35:45 +0800

    Remove useless stuff in index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,16 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import HomePage from "@/components/HomePage";
 import HowItWorks from "@/components/HowItWorks";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
     <div className="font-sans bg-white">
