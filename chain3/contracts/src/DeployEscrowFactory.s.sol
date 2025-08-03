3640f6d19b3d6f3205dde0b2469cf477b6daf5b9
lapsapthong-16 <honedwina@gmail.com>
2025-08-03 16:08:06 +0800

    modify escrowfactory contract, create script file

--- chain3/contracts/src/DeployEscrowFactory.s.sol
+++ chain3/contracts/src/DeployEscrowFactory.s.sol
@@ -3,8 +3,8 @@
 pragma solidity 0.8.23;
 
 import { Script } from "forge-std/Script.sol";
-import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import { EscrowFactory } from "contracts/EscrowFactory.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { EscrowFactory } from "cross-chain-swap/EscrowFactory.sol";
 
 // solhint-disable no-console
 import { console } from "forge-std/console.sol";
---
+++ chain3/examples/run.sh
@@ -0,0 +1,2 @@
+node chain3/deployment/serve-escrow.js
+node chain3/examples/cross-chain-complete.js --execute
