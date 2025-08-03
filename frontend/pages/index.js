29575ee5c066a333ee16041f25b6b98125263575
JingYuan <jingyuan0926@gmail.com>
2025-08-03 10:45:22 +0800

    Move header to landing page only

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@/components/Header";
 import HomePage from "@/components/HomePage";
 import HowItWorks from "@/components/HowItWorks";
 import Features from "@/components/Features";
@@ -6,6 +7,9 @@ import Features from "@/components/Features";
 export default function Home() {
   return (
     <div className="font-sans bg-white">
+      {/* Header */}
+      <Header />
+      
       {/* Home Section */}
       <section id="home">
         <HomePage />
